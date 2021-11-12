fca9374c2d14818a13e2bd972bf99ff18168be0e
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2021-11-12 18:55:38 +0530

    Delete QRCodeGenerator.py

--- QRCodeGenerator.py
+++
@@ -1,8 +0,0 @@
-import qrcode
-from random import randint
-from tkinter.filedialog import askopenfilename
-path=askopenfilename()
-with open(path,"rb") as f:
-    data=f.read()
-qr=qrcode.make(data)
-qr.save(f"qrcode_{randint(0,1000000)}.png")
